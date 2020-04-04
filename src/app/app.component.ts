6994fd8111ed7e1b9d3ae3bf9ec335cfe6906b74
Darlan <marangoni.darlan1@gmail.com>
2020-04-04 09:08:10 -0300

    configurando ngPrime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api/menuitem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngprime-app';
+  uploadedFiles: any[] = [];
+
+  items: MenuItem[];
+
+  home: MenuItem;
+
+  ngOnInit() {
+    this.items = [
+        {label:'Categories'},
+        {label:'Sports'},
+        {label:'Football'},
+        {label:'Countries'},
+        {label:'Spain'},
+        {label:'F.C. Barcelona'},
+        {label:'Squad'},
+        {label:'Lionel Messi', url: 'https://en.wikipedia.org/wiki/Lionel_Messi', icon: 'pi pi-external-link'}
+    ];
+    
+    this.home = {icon: 'pi pi-home'};
+}
+
+  constructor() {}
+
+  onUpload(event) {
+      for(let file of event.files) {
+          this.uploadedFiles.push(file);
+      }
+  }
 }
